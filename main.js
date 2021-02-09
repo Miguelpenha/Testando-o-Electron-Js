4684b573d299b10e1bc5c4e097cc9a3d8269bcb6
Miguelpenha <miguel.penha1@gmail.com>
2021-02-09 17:04:33 -0300

    teste

--- main.js
+++ main.js
@@ -24,3 +24,5 @@ app.on('activate', () => {
         createWindow()
     }
 })
+
+
---
+++ renderer.js
@@ -0,0 +1,8 @@
+const { ipcMain } = require('electron')
+
+ipcMain.on('ondragstart', (event, filePath) => {
+  event.sender.startDrag({
+    file: filePath,
+    icon: '/teste.txt'
+  })
+})
